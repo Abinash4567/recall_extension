a04b7be7126e331c6f2536f3e8e89cebc6a28626
Abinash4567 <abinashyadav4567@gmail.com>
2024-06-17 21:42:56 +0530

    fix: DOM adjustment

--- extension/content.js
+++ extension/content.js
@@ -1,88 +1,41 @@
 chrome.runtime.sendMessage({ type: "twitterOrNot" }, function (response) {
-    if (response.check) {
-        console.log("Yes");
-        window.onload = function () {
-            setInterval(() => {
-                const dd = document.querySelector("body > div:nth-of-type(1) > div > div > div:nth-of-type(2) > main > div > div > div > div:nth-of-type(1) > div > div:nth-of-type(5) > section > div > div");
+    if (response.check) 
+    {
+        console.log("Recall Extension Triggered!!!");
 
-                if(dd)
-                console.log(dd.childNodes.length);
+        setInterval(()=>{
+            const parentDiv = document.querySelector('#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > div.css-175oi2r.r-f8sm7e.r-13qz1uu.r-1ye8kvj > section > div > div');
 
-                let div = document.createElement('div');
-                div.innerHTML = `<body style="background-color: black; font-family: Sans-serif; margin: 2px; position: absolute;">
-        <div class="tweet" style="display: flex; width: min-content">
-            <div style="width: 40px; margin-right: 8px">
-                <img src="./assets/ayoub-moukhliss-423.jpeg" alt="avatar" width="40px" height="40px"
-                    style="border-radius: 100%" />
-            </div>
-    
-            <div>
-                <div style="display: flex; justify-content: space-between">
-                    <div style="display: flex">
-                        <span style="
-                    font-weight: 700;
-                    font-size: 15px;
-                    color: #e7e9ea;
-                    margin-right: 5px;
-                ">Abinash Ray</span>
-                        <span style="color: #797c7c; font-size: 15px; margin-right: 4px">@rayyadav9845</span>
-                        <span style="color: #797c7c; font-size: 15px">24 days</span>
-                    </div>
-    
-                    <!-- <div><img src="./assets/ots.png" alt="three_dot"></div> -->
-                </div>
-    
-                <div style="
-                color: #e7e9ea;
-                margin-top: 7px;
-                font-size: 15px;
-                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-                line-height: 20px;
-            ">
-                    This is my first Tweet. Thats it.lsdkvn Lorem ipsum dolor sit amet
-                    consectetur adipisicing elit. Illo iure ipsam, reprehenderit ipsum
-                    excepturi, ut illum necessitatibus molestiae atque animi alias officia
-                    facere! Necessitatibus reprehenderit eius facere totam. Cumque,
-                    tempore.
-                </div>
-    
-                <img src="./assets/pexels-vitalina-19392714.jpg" height="320px" width="520px"
-                    style="object-fit: cover; margin-top: 10px; border-radius: 2%" />
-    
-                <div style="
-                color: #71767b;
-                display: flex;
-                justify-content: space-between;
-                margin-top: 12px;
-            ">
-                    <div>
-                        <img src="./assets/comment.png" alt="commet"
-                            style="height: 18px; width: 18px; margin-right: 5px" /><span
-                            style="margin-right: 75px">12</span>
-                        <img src="./assets/repost.png" style="height: 15px; width: 18px; margin-right: 5px"
-                            alt="repost" /><span style="margin-right: 75px">12</span>
-                        <img src="./assets/love.png" alt="love" style="height: 16px; width: 18px; margin-right: 5px" /><span
-                            style="margin-right: 70px">12</span>
-                        <img src="./assets/impression.png" alt="imporession"
-                            style="height: 18px; width: 18px; margin-right: 5px" /><span
-                            style="margin-right: 70px">12</span>
-                    </div>
-    
-                    <div style="display: flex">
-                        <img src="./assets/bookmark.png" alt="bookmark"
-                            style="height: 18px; width: 18px; margin-right: 15px" />
-                        <img src="./assets/upload.png" alt="upload" style="height: 18px; width: 18px" />
-                    </div>
-                </div>
-            </div>
-        </div>`;
+            let arrYCoordinates = [];
+            parentDiv.childNodes.forEach((child)=>{
+                let individualChildY = child.attributes.style.value.split('(')[1].split('p')[0];
+                // console.log(individualChildY);
+                arrYCoordinates.push(individualChildY);
+            })
 
-            dd.appendChild(div);
-            }, 2000);
-        }
+            let count = [];
+            for(let i=0; i<arrYCoordinates.length-1; i++)
+            {
+                let check = arrYCoordinates[i+1] - arrYCoordinates[i] > 480 ? true : false;
+                if(check)
+                    count.push(i);
+            }
+
+            if(count.length > 0)
+            {
+                let expectedPosts = 0.2 * arrYCoordinates.length;
+                for(let i=0; i<count.length; i++)
+                {
+                    //replace Child
+                    expectedPosts--;
+                    if(expectedPosts===0)
+                        break;
+                }
+            }
+        }, 2000);
     }
     else 
-    {
+    {contain
         console.log("NO")
     }
-})
+});
